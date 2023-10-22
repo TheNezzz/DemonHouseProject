d54452ae8df4ab8018926cfb07f12c09f4cecc32
TheNezzz <heinonen.johannes@gmail.com>
2023-10-22 18:31:20 +0300

    built room and added lights

--- DH_Unity/Assets/Scripts/Demon.cs
+++ DH_Unity/Assets/Scripts/Demon.cs
@@ -10,44 +10,51 @@ public class Demon : MonoBehaviour
     public DemonState demonState = DemonState.Roaming;
     NavMeshAgent demonAI;
     public List<Transform> waypoints;
-    int nextWaypoint = 0;
+    int nextWaypoint;
     public float navThreshold = 1f;
     Rigidbody rb;
     Vector3 lastSeenPlayerPos;
     //float stunTimer = 0f;
     public float StunTimeout = 4f;
     DemonFrontSensor sensorFront;
-    DemonRearSensor sensorRear;
+    public float roamSpeed = 1;
+    public float chaseSpeed = 2f;
+    public float detectionTime = 2f;
+    public float detectionTimer = 0;
+
 
     private void Awake() {
         demonAI = GetComponent<NavMeshAgent>();
         rb = GetComponent<Rigidbody>();
         sensorFront = GetComponent<DemonFrontSensor>();
-        sensorRear = GetComponent<DemonRearSensor>();
+        nextWaypoint = Random.Range(0, waypoints.Count);
         demonAI.destination = waypoints[nextWaypoint].position;
-
+        
     }
     private void Update() {
         var playerVisible = sensorFront.TargetVisible();
 
         if (playerVisible == true) {
-            demonState = DemonState.Chasing;
-            lastSeenPlayerPos = sensorFront.target.position; 
+            detectionTimer += Time.deltaTime;
+            if (detectionTimer >= detectionTime) {
+                demonState = DemonState.Chasing;
+                lastSeenPlayerPos = sensorFront.target.position;
+                detectionTimer = 0f;
+            }
         }
     }
 
     private void FixedUpdate() {
 
         if (demonState == DemonState.Roaming) {
+            demonAI.speed = roamSpeed;
             if (Vector3.Distance(transform.position, waypoints[nextWaypoint].position) < navThreshold) {
-                nextWaypoint++;
-                if (nextWaypoint == waypoints.Count) {
-                    nextWaypoint = 0;
-                }
+                nextWaypoint = Random.Range(0, waypoints.Count);
                 demonAI.destination = waypoints[nextWaypoint].position;
             }
         }else if (demonState == DemonState.Chasing) {
-            demonAI.destination = lastSeenPlayerPos;
+            demonAI.speed = chaseSpeed;
+            demonAI.destination = sensorFront.target.position;
         }
     }    
     }
