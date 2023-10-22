3199cb501294861d14452e974e0e59d75efa51d2
LvnarSlvt <emma.jade.burton@gmail.com>
2023-10-22 18:26:58 +0300

    Ambient music added, footsteps in progress

--- DH_Unity/Assets/AudioManager.cs
+++ DH_Unity/Assets/AudioManager.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
     public AudioSource itemSounds;
-    
+    public List<AudioClip> footsteps;
+    public AudioSource footstepSource;
+    //public AudioSource Ambient;
+    //public List<AudioClip> ambientSounds;
+
     public void PlayItemSound(AudioClip clip) {
         itemSounds.PlayOneShot(clip);
     }
 
+    //public void PlayAmbientSounds() {
+    //    int index = Random.Range(0, ambientSounds.Count);
+    //    Ambient.PlayOneShot(ambientSounds[index]);
+    //}
+
+    public void PlayFootstepSound() {
+        int index = Random.Range(0, footsteps.Count);
+        footstepSource.PlayOneShot(footsteps[index]);
+    }
+
+    private void Update() {
+        //if (Input.GetKeyDown(KeyCode.F1)) { 
+        //    PlayFootstepSound();
+        //}
+    }
 }
